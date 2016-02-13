d3edd3b6fd935f5d4078bc809077767f1b025393
muji <freeformsystems@gmail.com>
2016-02-13 21:55:42 +0800

    Initial validation logic.

--- linkdown.js
+++ linkdown.js
@@ -1,5 +1,9 @@
 module.exports = {
   validate: {
+    // time to sleep between validation calls
+    // no sleep will get your fans going and pausing
+    // a while gives a change to see the errors
+    sleep: 5,
     fetch: {
       // this pattern will match the obvious file extensions
       // and treat directories with a trailing slash as index pages
@@ -14,7 +18,11 @@ module.exports = {
   crawl: {
     downloadUnsupported: false,
     userAgent: 'Linkdown',
+    // decrease interval and concurrency as normally
+    // executing against local server, adjust for your needs
     interval: 200,
-    maxConcurrency: 25
+    maxConcurrency: 25,
+    // increase in case we have a slow validation (unlikely)
+    listenerTTL: 30000
   }
 }
