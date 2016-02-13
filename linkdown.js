f682c00fa5da4b5d4e11cd1762dabbba257d3608
muji <freeformsystems@gmail.com>
2016-02-13 21:02:19 +0800

    Improve fetch condition for validate.

--- linkdown.js
+++ linkdown.js
@@ -2,9 +2,13 @@ module.exports = {
   validate: {
     fetch: {
       // this pattern will match the obvious file extensions
-      // and treat directories as index pages so that they 
-      // are also fetched
-      pattern: /(\.(htm|html)|(\/))$/i
+      // and treat directories with a trailing slash as index pages
+      // so that they are also fetched
+      file: /(\.(htm|html)|(\/))$/i,
+
+      // this will catch paths without a trailing slash and no 
+      // file extension, this pattern is negated in the fetch condition!
+      directory: /\..+$/i
     }
   },
   crawl: {
