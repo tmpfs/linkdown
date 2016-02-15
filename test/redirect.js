82c0188f7b882e43d67119e1332881ef63d7d8f5
muji <freeformsystems@gmail.com>
2016-02-15 16:09:40 +0800

    Add redirect isolation test.

---
+++ test/redirect.js
@@ -0,0 +1,45 @@
+var http = require('http')
+  , port = 9000
+  , Crawler = require('simplecrawler');
+
+/**
+ *  Test to verify automatically following redirects.
+ */
+
+function request(req, res) {
+  //console.log(req.url);
+
+  var buf;
+  if(req.url === '/') {
+    buf = new Buffer(
+      '<html><body><a href="/redirect">link</a></body></html>');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Content-Length', buf.length);
+    res.end(buf);
+  }else if(req.url === '/redirect') {
+    res.writeHead(301, {'Location': '/page'})
+    res.end();
+  }else if(req.url === '/page') {
+    buf = new Buffer(
+      '<html><body><a href="/">home</a></body></html>');
+    res.setHeader('Content-Type', 'text/html; charset=utf-8');
+    res.setHeader('Content-Length', buf.length);
+    res.end(buf);
+  }
+}
+
+var server = http.createServer(request);
+
+server.listen(port, function() {
+  //console.log('server on %s', port);
+
+  var crawler = new Crawler('localhost', '/', port);
+
+  // i would expect this to fire for / 
+  crawler.on('fetchcomplete', function(item/*, buf, res*/) {
+    console.log('fetch complete fired %s', item.url); 
+  })
+
+  crawler.start();
+
+})
