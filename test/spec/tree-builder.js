428f8902a6c9c14eba7966f10b1cc8aa90d1f644
muji <freeformsystems@gmail.com>
2016-02-18 12:56:00 +0800

    Improve tree builder logic and tests.

--- test/spec/tree-builder.js
+++ test/spec/tree-builder.js
@@ -1,8 +1,21 @@
 var expect = require('chai').expect
   , fs = require('fs')
-  , archy = require('archy')
+  //, archy = require('archy')
   , builder = require('../../lib/tree-builder');
 
+
+/**
+ *  Helper to label the tree items, makes for more predictable
+ *  assertions.
+ */
+function label(item, root){
+  return root
+    ? item.pathname + ' (' + item.hostname + ')' : '/' + item.name;
+}
+
+/**
+ *  Helper to load the mock fixtures.
+ */
 function load(file) {
   var contents = '' + fs.readFileSync(file)
     , lines = contents.trim().split('\n');
@@ -22,47 +35,50 @@ function load(file) {
  *  NODE_ENV=devel ldn x http://localhost:8080/meta \
  *    --cmd 'ldn meta' --json > test/fixtures/simple.json.log
  *
+ *  Build the deep fixture:
+ *
+ *  NODE_ENV=devel ldn x http://localhost:8080/into/the/deep \
+ *    --cmd 'ldn meta' --json > test/fixtures/deep.json.log
  *
  *  Build the mock fixture: 
  *
  *  NODE_ENV=devel ldn x http://localhost:8080 \
  *    --cmd 'ldn meta' --json > test/fixtures/mock.json.log
  */
-
 describe('tree-builder:', function() {
 
-  //it('should build simple tree', function(done) {
-    //var input = load('test/fixtures/simple.json.log')
-      //, res = builder(input)
-      //, keys = Object.keys(res)
-      //, host
-      //, node;
-    //expect(keys.length).to.eql(1);
-    //host = res['localhost:8080'];
-    //expect(host.label).to.be.a('string');
-    //expect(host.nodes).to.be.an('array');
-    //node = host.nodes[0];
-    //expect(node).to.be.an('object');
-    //expect(node.label).to.be.a('string').to.eql('Meta Page');
-    //done();
-  //});
+  it('should build simple tree', function(done) {
+    var input = load('test/fixtures/simple.json.log')
+      , res = builder(input)
+      , keys = Object.keys(res)
+      , host
+      , node;
+    expect(keys.length).to.eql(1);
+    host = res['localhost:8080'];
+    expect(host.label).to.be.a('string');
+    expect(host.nodes).to.be.an('array');
+    node = host.nodes[0];
+    expect(node).to.be.an('object');
+    expect(node.label).to.be.a('string').to.eql('Meta Page');
+    done();
+  });
 
-  //it('should build simple tree w/ standard port', function(done) {
-    //var input = load('test/fixtures/simple.json.log');
-    //input[0].port = 443;
-    //var res = builder(input)
-      //, keys = Object.keys(res)
-      //, host
-      //, node;
-    //expect(keys.length).to.eql(1);
-    //host = res.localhost;
-    //expect(host.label).to.be.a('string');
-    //expect(host.nodes).to.be.an('array');
-    //node = host.nodes[0];
-    //expect(node).to.be.an('object');
-    //expect(node.label).to.be.a('string').to.eql('Meta Page');
-    //done();
-  //});
+  it('should build simple tree w/ standard port', function(done) {
+    var input = load('test/fixtures/simple.json.log');
+    input[0].port = 443;
+    var res = builder(input)
+      , keys = Object.keys(res)
+      , host
+      , node;
+    expect(keys.length).to.eql(1);
+    host = res.localhost;
+    expect(host.label).to.be.a('string');
+    expect(host.nodes).to.be.an('array');
+    node = host.nodes[0];
+    expect(node).to.be.an('object');
+    expect(node.label).to.be.a('string').to.eql('Meta Page');
+    done();
+  });
 
   //it('should build simple tree w/ url label', function(done) {
     //var input = load('test/fixtures/simple.json.log');
@@ -81,50 +97,72 @@ describe('tree-builder:', function() {
     //done();
   //});
 
-  //it('should build simple tree w/ hostMap', function(done) {
-    //var input = load('test/fixtures/simple.json.log')
-      //, hostMap = {'localhost:8080': 'example.com'}
-      //, res = builder(input, {hostMap: hostMap})
-      //, keys = Object.keys(res)
-      //, host
-      //, node;
+  it('should build simple tree w/ hostMap', function(done) {
+    var input = load('test/fixtures/simple.json.log')
+      , hostMap = {'localhost:8080': 'example.com'}
+      , res = builder(input, {hostMap: hostMap})
+      , keys = Object.keys(res)
+      , host
+      , node;
 
-    //expect(keys.length).to.eql(1);
-    //host = res['example.com'];
-    //expect(host.label).to.be.a('string');
-    //expect(host.nodes).to.be.an('array');
-    //node = host.nodes[0];
-    //expect(node).to.be.an('object');
-    //expect(node.label).to.be.a('string').to.eql('Meta Page');
-    //done();
-  //});
-
-  it('should build mock tree', function(done) {
-    var input = load('test/fixtures/mock.json.log')
-      , label = function(item, root){
-          return root
-            ? item.pathname + ' (' + item.hostname + ')' : item.pathname;
-        }
+    expect(keys.length).to.eql(1);
+    host = res['example.com'];
+    expect(host.label).to.be.a('string');
+    expect(host.nodes).to.be.an('array');
+    node = host.nodes[0];
+    expect(node).to.be.an('object');
+    expect(node.label).to.be.a('string').to.eql('Meta Page');
+    done();
+  });
+  
+  it('should build tree with no parents', function(done) {
+    var input = load('test/fixtures/deep.json.log')
       , res = builder(input, {lean: true, label: label})
       , keys = Object.keys(res)
       , host
       , node;
     expect(keys.length).to.eql(1);
     host = res['localhost:8080'];
-    expect(host.label).to.be.a('string');
+    expect(host.label).to.be.a('string')
+      .to.eql('/ (localhost:8080)');
+
     expect(host.nodes).to.be.an('array');
-    //console.dir(host.nodes.length);
 
-    //host.nodes.forEach(function(node) {
-      //console.log(node.label);
-    //})
+    node = host.nodes[0];
+    expect(node.label).to.eql('/into');
 
-    console.dir(res, {depth: 4});
+    node = node.nodes[0];
+    expect(node.label).to.eql('/the');
 
-    console.log(archy(res['localhost:8080']));
-    //node = host.nodes[0];
-    //expect(node).to.be.an('object');
-    //expect(node.label).to.be.a('string').to.eql('Meta Page');
+    node = node.nodes[0];
+    expect(node.label).to.eql('/deep');
+
+    //console.dir(res, {depth: 4});
+    //console.log(archy(res['localhost:8080']));
+
+    done();
+  });
+
+
+  it('should build full tree', function(done) {
+    var input = load('test/fixtures/mock.json.log')
+      , res = builder(input, {lean: true, label: label})
+      , keys = Object.keys(res)
+      , host
+      , node;
+    expect(keys.length).to.eql(1);
+    host = res['localhost:8080'];
+    expect(host.label).to.be.a('string')
+      .to.eql('/ (localhost:8080)');
+
+    expect(host.nodes).to.be.an('array');
+
+    node = host.nodes[0];
+    expect(node.label).to.eql('/meta');
+
+    //console.dir(res, {depth: 4});
+    //console.log(archy(res['localhost:8080']));
+
     done();
   });
 
