79988e65ad1b98bec17ae1d6b301a54e1124e6e0
muji <freeformsystems@gmail.com>
2016-02-22 12:02:35 +0800

    Assert on exec command.

--- test/global.js
+++ test/global.js
@@ -1,5 +1,7 @@
 process.env.PORT = process.env.PORT || 8080;
 process.env.URL = process.env.URL || 'http://localhost:' + process.env.PORT;
+// ensure we can find our own executables
+process.env.PATH = './bin:' + process.env.PATH;
 var server = require('./server/app');
 before(function(done) {
   server.listen(process.env.PORT, function() {
--- test/spec/exec.js
+++ test/spec/exec.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect
+  , fs = require('fs')
   , linkdown = require('../../lib/linkdown')
   , pkg = require('../../package.json')
   , argv = require('../argv')
@@ -37,24 +38,74 @@ describe('exec:', function() {
   });
 
   it('should execute external command', function(done) {
+    var output = 'target/exec-cat.log';
     var cli = linkdown(pkg, pkg.name)
       , args = argv(
-        ['exec', process.env.URL + '/meta', '--cmd', 'cat']);
+        [
+          'exec', process.env.URL + '/meta', '--cmd', 'cat',
+          '-o=' + output
+        ]);
     cli.parse(args, function complete(res) {
       expect(res.errors.list.length).to.eql(0);
+      var contents = '' + fs.readFileSync(output);
+      expect(contents).to.eql(
+        '' + fs.readFileSync('test/fixtures/meta-page.html'));
       done(); 
     });
   });
 
-  it('should execute external command w/ argument', function(done) {
+  it('should execute external command w/ --json', function(done) {
+    var output = 'target/exec-echo.log';
+    var cli = linkdown(pkg, pkg.name)
+      , args = argv(
+        [
+          'exec', process.env.URL + '/meta', '--cmd', 'echo',
+          '-o=' + output, '--json'
+        ]);
+    cli.parse(args, function complete(res) {
+      expect(res.errors.list.length).to.eql(0);
+      var contents = '' + fs.readFileSync(output)
+        , data = JSON.parse(contents);
+      expect(data).to.be.an('object');
+      expect(data.url).to.eql('http://localhost:8080/meta');
+      done(); 
+    });
+  });
+
+
+  it('should execute external command w/ arguments', function(done) {
+    var output = 'target/exec-grep.log';
     var cli = linkdown(pkg, pkg.name)
       , args = argv(
         [
           'exec', process.env.URL + '/meta',
-          '--cmd', 'grep --color=never meta'
+          '--cmd', 'grep --color=never meta',
+          '-o=' + output
         ]);
     cli.parse(args, function complete(res) {
       expect(res.errors.list.length).to.eql(0);
+      var contents = '' + fs.readFileSync(output);
+      expect(contents).to.eql(
+        '' + fs.readFileSync('test/fixtures/meta-grep.log'));
+      done(); 
+    });
+  });
+
+  it('should execute external command w/ -- (skip args)', function(done) {
+    var output = 'target/exec-grep.log';
+    var cli = linkdown(pkg, pkg.name)
+      , args = argv(
+        [
+          'exec', process.env.URL + '/meta',
+          '--cmd', 'grep',
+          '-o=' + output,
+          '--', '--color=never', 'meta'
+        ]);
+    cli.parse(args, function complete(res) {
+      expect(res.errors.list.length).to.eql(0);
+      var contents = '' + fs.readFileSync(output);
+      expect(contents).to.eql(
+        '' + fs.readFileSync('test/fixtures/meta-grep.log'));
       done(); 
     });
   });
